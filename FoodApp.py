5fc6d15bb76f5b1b13d93cca693fe8c1d8090cd2
AKASH A J <akashjayanthi2019@gmail.com>
2024-06-26 13:06:59 +0530

    authentication and authorization template created

--- FoodApp.py
+++ FoodApp.py
@@ -1,16 +1,40 @@
 class LoginSystem:
-    pass
+
+    def Login(self):
+        pass
+
+    def Register(self):
+        pass
+
+    def GuestLogin(self):
+        pass
+
+    def ValidateOption(self, option):
+        if option==1:
+            self.Login()
+        elif option == 2:
+            self.Register()
+        elif option==3:
+            self.GuestLogin()
+        else:
+            print("Thank you, visit again😊")
+            exit()
 
 
 class FoodApp:
-    LoginOptions={1:"Login", 2: "Register", 3:"Guest", 4: "Exit \n"}
+    LoginOptions = {1: "Login", 2: "Register", 3: "Guest", 4: "Exit \n"}
+
     @staticmethod
     def Init():
         print("--Welcome to Online Food Ordering--")
+
+        loginSystem=LoginSystem()
+
         for option in FoodApp.LoginOptions:
             print(f"{option}.{FoodApp.LoginOptions[option]}", end=" ")
-        choice=int(input("Enter your choice: "))
-        if 1<=choice<=4:
+        choice = int(input("Enter your choice: "))
+        if 1 <= choice <= 4:
             print(f"Your choice is => {FoodApp.LoginOptions[choice]}")
+            loginSystem.ValidateOption(choice)
         else:
             print("Invalid Choice")
